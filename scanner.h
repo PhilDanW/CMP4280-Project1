74c2eb89128b3e675ee413069c9a33d9024f61d9
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-03-08 17:21:31 -0600

    Create scanner.h

---
+++ scanner.h
@@ -0,0 +1,22 @@
+#ifndef SCANNER_H
+#define SCANNER_H
+
+#define row 21
+#define col 24
+	 
+int table[row][col];
+
+extern int z;
+char error[LIMIT][LIMIT];
+char errorBuffer[LIMIT];
+extern int errorIndex;
+int isError;
+
+void mainDriver(char *array, int line);
+void ignoreComments(char *array, int line);
+void checkError();
+int isCharacter(char c);
+void checkIfKeyword();
+void fsaTable();
+
+#endif
