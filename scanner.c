22844804940d3d687d464ab48c9245a5c8292f26
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-03-08 15:23:45 -0600

    Create scanner.c

---
+++ scanner.c
@@ -0,0 +1,8 @@
+/************************
+* Author: Philip Wright *
+* Project 1: Scanner    *
+* scanner.c loops through the FSA table and begins storing *
+* tokens in the token struct                               *
+***********************************************************/
+
+void mainDriver(char *
