28fbecb72b88de3ecec54d1b4dbccd2417620133
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-04-11 18:04:39 -0500

    Update testScanner.c

--- testScanner.c
+++ testScanner.c
@@ -4,98 +4,38 @@
 * testScanner.c asks for one token at a time to display*
 * the token to the screen *
 *******************************************************/
-
-#include "token.h"
 #include "scanner.h"
-#include "stdio.h"
-#include "stdlib.h"
 #include "testScanner.h"
+#include <iostream>
+#include <iomanip>
 
-//test scanner
-void testScanner(){
-	int i;
-  	int j;
-	
-	if (isError == 1) {
-		for(j = 0; j < errorIndex; j++){
-			printf("%s\n", error[j]);
+//array holding all the names of the allowed tokens
+std::string tokens[] = {"BEGIN_TOKEN","END_TTOKEN","LOOP_TOKEN","WHILE_TOKEN", "VOID_TOKEN", "EXIT_TOKEN", "GETTER_TOKEN", "OUTTER_TOKEN", "MAIN_TOKEN", 
+			"IF_TOKEN", "THEN_TOKEN", "ASSIGN_TOKEN", "DATA_TOKEN", "PROC_TOKEN", "EQUAL_TOKEN", "EQUAL_LESS_THAN_TOKEN", 
+			"EQUAL_GREATER_THAN_TOKEN", "EQUAL_EQUAL_TOKEN", "COLON_TOKEN", "COLON_EQUAL_TOKEN", "ADD_TOKEN", "SUBTRACT_TOKEN", 
+			"ASTERISK_TOKEN", "DIVIDE_TOKEN", "MODULUS_TOKEN", "PERIOD_TOKEN", "LEFT_PARENT_TOKEN", "RIGHT_PARENT_TOKEN", "COMMA_TOKEN", 
+			"LEFT_BRACE_TOKEN", "RIGHT_BRACE_TOKEN", "SEMICOLON_TOKEN", "LEFT_BRACKET_TOKEN", "RIGHT_BRACKET_TOKEN", "ID_TOKEN", 
+			"INT_TOKEN", "EOF_TOKEN", "ERROR_TOKEN"};
+
+//test scanner to loop thru tokens from given file or keyboard input
+void testScanner(std::ifstream& file){
+	int lineNum = 1;
+	do {
+		//get all the tokens from the given file
+		Token token = scanner(file, lineNum);
+		if(token.tokenID == ERROR_TOKEN) {
+			exit(EXIT_FAILURE);
 		}
-	} 
-  	else {
-		//since my token type is enum, I have to print them all manually
-		for(i=0; i < z; i++){
-		
-			if(tokens[i].tokenType == IDENTIFIER){
-				printf("Line: %-3d | Token: %-8s | Token Type: Identifier Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == INT){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Number Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == EQUAL){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Equal Token\n", tokens[i].lineNum, tokens[i].instance);	
-			}  else if(tokens[i].tokenType == LESS_THAN){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Less Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == GREATER_THAN){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Greather Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == COLON){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Colon Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == PLUS){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Plus Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == MINUS){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Minus Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == STAR){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Asterik Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == SLASH){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Slash Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == PERCENT){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Percent Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == DOT){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Dot Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == LEFT_PARENT){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Left Parenthesis Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == RIGHT_PARENT){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Right Parenthesis Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == COMMA){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Comma Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == LEFT_BRACE){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Left Brace Token\n", tokens[i].lineNum, tokens[i].instance);	
-			}  else if(tokens[i].tokenType == RIGHT_BRACE){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Right Brace Token\n", tokens[i].lineNum, tokens[i].instance);	
-			}  else if(tokens[i].tokenType == SEMICOLON){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Semicolon Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == LEFT_BRACKETS){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Left Brackets Token\n", tokens[i].lineNum, tokens[i].instance);
-			}  else if(tokens[i].tokenType == RIGHT_BRACKETS){	
-				printf("Line: %-3d | Token: %-8s | Token Type: Right Brackets Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == BEGIN_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: begin Keyword Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == END_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: end Keyword Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == LOOP_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: loop Keyword Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == WHILE_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: while Keyword Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == VOID_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: void Keyword Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == EXIT_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: exit Keyword Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == GETTER_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: getter Keyword Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == OUTTER_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: outter Keyword Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == MAIN_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: main Keyword Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == IF_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: if Keyword Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == THEN_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: then Keyword Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == ASSIGN_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: assign Keyword Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == DATA_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: data Keyword Token\n", tokens[i].lineNum, tokens[i].instance);	
-			} else if(tokens[i].tokenType == PROC_KEYWORD){		
-				printf("Line: %-3d | Token: %-8s | Token Type: proc Keyword Token\n", tokens[i].lineNum, tokens[i].instance);
-			} else if(tokens[i].tokenType == EOT){		
-				printf("Line: %-3d | Token: %-8s | Token Type: EOF Token\n", tokens[i].lineNum, tokens[i].instance);
-			}
+		else {
+			tokenInfo(token);
 		}
-	}
+	} while (file);
+}
+
+//prints out the token info to the screen
+void tokenInfo(Token token) {
+	std::cout << std::left;
+	std::cout << "Line number: " << std::setw(3) << token.lineNum;
+	std::cout << "\tToken type: " << std::setw(22) << tokens[token.tokenID];
+	std::cout << "\tToken value: " << std::setw(10) << token.tokenString << std::endl;
 }
