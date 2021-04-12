f862f4f65c4844f7b2e3e213dad1859a7280465e
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-04-12 14:25:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,14 @@
 #include <fstream>
 #include "scanner.h"
 
+std::string tokens[] = {"BEGIN_TK","END_TK","LOOP_TK","WHILE_TK", "VOID_TK", "EXIT_TK", "GETTER_TK", "OUTTER_TK", "MAIN_TK", 
+                        "IF_TK", "THEN_TK", "ASSIGN_TK", "DATA_TK", "PROC_TK", "EQUALS_TK", "EQUALS_OR_LESS_THAN_TK", 
+                        "EQUALS_OR_GREAT_THAN_TK", "EQUALS_EQUALS_TK", "COLON_TK", "COLON_EQUALS_TK", "PLUS_TK", "MINUS_TK", 
+                        "ASTERISK_TK", "SLASH_TK", "PERCENT_TK", "PERIOD_TK", "LEFT_PAREN_TK", "RIGHT_PAREN_TK", "COMMA_TK", 
+                        "LEFT_BRACE_TK", "RIGHT_BRACE_TK", "SEMI_COLON_TK", "LEFT_BRACKET_TK", "RIGHT_BRACKET_TK", "ID_TK", 
+                        "INT_TK", "EOF_TK", "ERROR_TK"
+};
+
 int main(int argc, char** argv) {
     std::ifstream file;         // input file
     std::string fileName;         // filename
