7fac10c4372a41d6a7b0b983045a1e9556c9029b
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-04-12 14:19:49 -0500

    Update scanner.cpp

--- scanner.cpp
+++ scanner.cpp
@@ -41,6 +41,15 @@ int fsa_table[23][23] = {   // [row] [col]   ws = whitespace, lc = lowercase, UC
     {121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121}  // 22 ]
 };
 
+
+std::string tokens[] = {"BEGIN_TK","END_TK","LOOP_TK","WHILE_TK", "VOID_TK", "EXIT_TK", "GETTER_TK", "OUTTER_TK", "MAIN_TK", 
+                        "IF_TK", "THEN_TK", "ASSIGN_TK", "DATA_TK", "PROC_TK", "EQUALS_TK", "EQUALS_OR_LESS_THAN_TK", 
+                        "EQUALS_OR_GREAT_THAN_TK", "EQUALS_EQUALS_TK", "COLON_TK", "COLON_EQUALS_TK", "PLUS_TK", "MINUS_TK", 
+                        "ASTERISK_TK", "SLASH_TK", "PERCENT_TK", "PERIOD_TK", "LEFT_PAREN_TK", "RIGHT_PAREN_TK", "COMMA_TK", 
+                        "LEFT_BRACE_TK", "RIGHT_BRACE_TK", "SEMI_COLON_TK", "LEFT_BRACKET_TK", "RIGHT_BRACKET_TK", "ID_TK", 
+                        "INT_TK", "EOF_TK", "ERROR_TK"
+};
+
 // Map for Keywords
 std::map<std::string, tokens> keywords = {
     {"begin", BEGIN_TK},
