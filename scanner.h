7a41dc959f4362442a7234b2a5ebc1cdaeb514b5
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-04-12 11:52:34 -0500

    Update scanner.h

--- scanner.h
+++ scanner.h
@@ -6,10 +6,11 @@
 #include <map>
 #include <string>
 
+void testScanner(std::ifstream& file);
 Token scanner(std::ifstream& in_file, int& lineNum);
 
 int getColumn(char the_char);
-
 Token setToken(int state, std::string word, int lineNum);
+void tokenInfo(Token token);
 
 #endif
